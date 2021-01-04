7c7732be89292b97413fe66bd8f60829001ed6be
Pedro Pedrosa <pshpedro@gmail.com>
2021-01-04 17:05:25 -0400

    Foram criados os metodos de lista

--- index.php
+++ index.php
@@ -7,9 +7,22 @@ $sql = new Sql();
 $consulta_usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($consulta_usuarios);*/
 
-// Exemplo PDO - DAO - SELECT
+/* Exemplo PDO - DAO - SELECT
 $id = new Usuario();
 $id->loadById(6);
-echo $id;
+echo $id;*/
+
+/* Exemplo PDO - DAO - LISTA 1
+$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+/* Exemplo PDO - DAO - LISTA 2
+$busca = Usuario::search("a");
+echo json_encode($busca);*/
+
+// Exemplo PDO - DAO - LISTA 3 - Passando o login
+$usuario = new Usuario();
+$usuario->login("Predo","12345");
+echo $usuario;
 
 ?>
